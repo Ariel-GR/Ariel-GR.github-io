0236801dd8b47a45d3d8d68840f968a81c9e344e
Ariel <75487262+Ariel-GR@users.noreply.github.com>
2023-04-11 20:44:56 -0300

    Modificaciones generales
    Se realizo el re-ordenamiento de los elementos, cambio de fuente, modificación de la cabecera, se cambio el formato de la galería de imágenes.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-let siliderWrap = document.querySelector('.img-conteiner');
-let slider = document.querySelector('.slider-conteiner');
-let clones = [];
-
-let items = [...document.querySelectorAll('.slider-img')];
-let images = [...document.querySelectorAll('.img-div')];
-
-images.forEach((image, idx)=>{
-    image.style.backgroundImage = `url(./img/${idx+1}.jpg)`
-})
-
-items.forEach(item=>{
-    let clone = item.cloneNode(true);
-    clone.classList.add('clone');
-    slider.appendChild(clone);
-    clones.push(clone);
-})
-
 function closeFuction(x) {
     x.classList.toggle("change");
 }
@@ -28,7 +10,6 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
   let itemsAbout = [...document.querySelectorAll('.imgDuo')];
   let imagesAbout = [...document.querySelectorAll('.img-cont')];
   
